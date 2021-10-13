a52d03ba79441fb7879d3f8aacace9084683aaf0
Dirichi <din206@nyu.edu>
2021-10-13 19:59:05 +0100

    Add dummy player

---
+++ src/OneAD/players/DummyPlayer.java
@@ -0,0 +1,27 @@
+package OneAD.players;
+
+import core.GameState;
+import players.Player;
+import utils.Types;
+
+public class DummyPlayer extends Player {
+    public DummyPlayer(int pId) {
+        super(0, pId);
+    }
+
+    @Override
+    public Types.ACTIONS act(GameState gs) {
+        return Types.ACTIONS.ACTION_STOP;
+    }
+
+    @Override
+    public int[] getMessage() {
+        // default message
+        return new int[Types.MESSAGE_LENGTH];
+    }
+
+    @Override
+    public Player copy() {
+        return new OneAD.players.DummyPlayer(playerID);
+    }
+}
